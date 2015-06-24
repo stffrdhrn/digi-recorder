7877d7d8b34ce7940426768e16572a7059eafba7
Stafford <shorne@gmail.com>
2015-06-24 22:22:09 +0900

    start implementing state changes and address counting

--- rtl/drec_controller.v
+++ rtl/drec_controller.v
@@ -26,10 +26,87 @@ module drec_controller (
   sdram_rd_enable,
   sdram_rd_rdy,
   
-  button,
+  play_btn, rec_btn,
   
   clk, rst_n
 );
+
+output[15:0] dac_data;
+output       dac_enable;
+
+input [15:0] adc_data;   // every 44,000 hz
+output       adc_enable;
+
+output[15:0] sdram_wr_data;
+output[23:0] sdram_wr_addr;
+output       sdram_wr_enable;
+
+input [15:0] sdram_rd_data;
+output[23:0] sdram_rd_addr;
+output       sdram_rd_enable;
+input        sdram_rd_rdy;
+
+input        play_btn;
+input        rec_btn;
+input        clk;
+input        rst_n;
+
+reg  [15:0]  dac_data;   // every 44,000 hz
+reg          dac_enable; 
+
+reg          adc_enable;
+
+reg  [1:0]   state;
+wire  [1:0]  next;
+
+reg [23:0] sdram_addr_r;
+
+localparam   IDLE   = 2'b00,
+             PLAY   = 2'b01,
+             RECORD = 2'b10;
+
+/* Handle button presses and state changes */             
+always @ (*) 
+case (state)
+  IDLE:
+   if (play_btn)
+     next = PLAY;
+   else if (rec_btn)
+     next = RECORD;
+   else 
+     next = IDLE;
+  PLAY:
+   if (play_btn | rec_btn)
+     next = IDLE;
+   else 
+     next = PLAY;
+  RECORD:
+   if (play_btn | rec_btn)
+     next = IDLE;
+   else 
+     next = RECORD;
+  default:
+    next = IDLE;
+endcase
+             
+always @ (posedge clk)
+if (~rst_n)
+  state <= IDLE;
+else 
+  state <= next;
+
+
+always @ (posedge clk)
+if (~rst_n)
+  sdram_addr_r <= 24'd0;
+else 
+  case(state)
+    PLAY, RECORD:
+     sdram_addr_r <= sdram_addr_r + 1'd1;
+    default:
+     sdram_addr_r <= 24'd0;
+  
+  endcase
 
 
 endmodule
